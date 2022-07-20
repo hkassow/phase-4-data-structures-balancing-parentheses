af9c61789e24997900c1ffeb28b2100f4f71be65
Hunter Kassow <hunterkassow@protonmail.com>
2022-07-20 11:06:49 -0400

    commit tree 63a326dc9f13663bb512a3e7555d9ecc61d6b171

--- lib/balancing_parentheses.rb
+++ lib/balancing_parentheses.rb
@@ -1,3 +1,18 @@
 require_relative './stack'
 
-# your code here
+def balancing_parentheses(string)
+    ans = 0
+    stack = Stack.new()
+    string.each_char{|x|
+        if x == '('
+            stack.push x
+        elsif stack.size == 0
+            ans += 1
+        else
+            stack.pop
+        end
+    } 
+    ans += stack.size
+end
+
+puts balancing_parentheses("())()")
